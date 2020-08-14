1d6e8c464a7ee385dbd4366a9ec37c05dd717a7f
Vanildo Júnior <vanildojr33@gmail.com>
2020-08-13 22:48:48 -0300

    Terceira aula completa

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -3,32 +3,49 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css'
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const  TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection(){
+    api.post('connections', {
+      user_id: teacher.id
+    })
+  };
+
   return(
     <article className="teacher-item">
           <header>
-            <img src="https://www.leninja.com.br/wp-content/themes/LeNinja/images/ninja-negro-footer.png" alt="Geogea"/>
+            <img src={teacher.avatar} alt={teacher.name}/>
             <div>
-              <strong>Geogea</strong>
-              <span>Português</span>
+              <strong>{teacher.name}</strong>
+              <span>{teacher.subject}</span>
             </div>
           </header>
-          <p>
-            lakdakdsnalsnalfnadkfnaldnld
-            <br />
-            nsdajsndjabsjbfaf
-          </p>
+          <p>{teacher.bio}</p>
 
           <footer>
             <p>
               Preço/hora 
-              <strong>R$50,00</strong>
+              <strong>R$ {teacher.cost} </strong>
             </p>
-            <button type="button">
+            <a rel="noopener noreferrer" target="_blank" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
               <img src={whatsappIcon} alt="Whatsapp"/>
               Entrar em contato
-            </button>
+            </a>
           </footer>
         </article>
   );
